137949b217611f6aa7e83ba09b040b4cf99338d7
golmschenk <greg@olmschenk.com>
2024-01-04 14:37:29 -0500

    Try with pyo3 linking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::{Python, PyModule, PyResult};
+
+fn main() -> PyResult<()> {
+    pyo3::prepare_freethreaded_python();
+    Python::with_gil(|py| {
+        println!("Message from Rust.");
+        let python_module = PyModule::import(py, "python_library.python_module")?;
+        let args = (3, 4.5);
+        let product: f64 = python_module.getattr("multiply")?.call1(args,)?.extract()?;
+        println!("Product in Rust is {}.", product);
+        Ok(())
+    })
 }
